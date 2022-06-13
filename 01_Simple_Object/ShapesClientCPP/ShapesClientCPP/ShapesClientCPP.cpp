de2cfc042be5eb81581bbd4698173326fa72adab
songpeng22 <songpeng24@msn.com>
2022-06-13 16:47:50 +0800

    simple object: client app to call ATL

---
+++ 01_Simple_Object/ShapesClientCPP/ShapesClientCPP/ShapesClientCPP.cpp
@@ -0,0 +1,33 @@
+// ShapesClientCPP.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <objbase.h>
+
+#include "../../Shapes/Shapes/Shapes_i.c"
+#include "../../Shapes/Shapes/Shapes_i.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    CoInitializeEx(0,COINIT_MULTITHREADED);
+
+    ICircle * pCircle;
+    HRESULT hr = CoCreateInstance(CLSID_Circle,0,CLSCTX_INPROC_SERVER,IID_ICircle,(LPVOID*)&pCircle);
+
+    if(SUCCEEDED(hr))
+    {
+        pCircle->AddRef();
+        pCircle->put_Radius(3.0);
+        double dArea;
+        pCircle->GetArea(&dArea);
+        std::cout << dArea << std::endl;
+        getchar();
+        pCircle->Release();
+    }
+
+    CoUninitialize();
+
+	return 0;
+}
+
